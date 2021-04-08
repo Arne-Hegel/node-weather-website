479a6752730816976b4b9a2af216cc8102e462e5
Hegel <HegelAr@teamtraining.local>
2021-04-08 10:21:59 +0200

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    const location = search.value
+    const url = 'http://localhost:3000/weather?address=' + location
+
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                messageOne.textContent = data.error
+            } else{
+                messageOne.textContent = 'Location: ' + data.location
+                messageTwo.textContent = 'Forecast: ' + data.weather_descriptions + '; Temperature: ' + data.temperature + '; Feelslike: ' + data.feelslike
+            }
+        })
+    })
+})
